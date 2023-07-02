aca5a2ea93bb77dc49ee5f74041c20e708780061
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-07-02 21:21:45 +0800

    Change class name to Form Application

---
+++ src/main/java/com/example/first_desktop_app/FormApplication.java
@@ -0,0 +1,26 @@
+package com.example.first_desktop_app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FormApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FormApplication.class.getResource("form-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        //scene.getStylesheets().add("main.css");
+        scene.getStylesheets().add(FormApplication.class.getResource("main.css").toExternalForm());
+       // scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+        stage.setTitle("Form");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
