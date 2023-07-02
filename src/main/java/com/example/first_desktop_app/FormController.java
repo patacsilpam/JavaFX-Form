838ebf63d3f5551f01bf758c6bb4ebf30e75d84f
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-07-02 21:26:59 +0800

    Changed class name to FormController

---
+++ src/main/java/com/example/first_desktop_app/FormController.java
@@ -0,0 +1,28 @@
+package com.example.first_desktop_app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+
+public class FormController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private Circle circle;
+
+
+    public void getName(ActionEvent actionEvent) {
+        String newFname = firstName.getText();
+        String newLname = lastName.getText();
+        welcomeText.setText(newFname + " " + newLname);
+
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+    }
+}
