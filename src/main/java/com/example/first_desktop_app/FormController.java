6379b6d37e795a0d5773e6aa929e34810087192c
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-07-05 23:24:19 +0800

    Add switch scene between form-view.fxml and login-view..fxml

--- src/main/java/com/example/first_desktop_app/FormController.java
+++ src/main/java/com/example/first_desktop_app/FormController.java
@@ -3,12 +3,15 @@ package com.example.first_desktop_app;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FormController {
     @FXML
@@ -20,7 +23,28 @@ public class FormController {
     @FXML
     private Circle circle;
 
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
 
+
+   /* private void  switchToLoginForm(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void  switchToSignUpForm(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("form-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }*/
+
+    /*
     public void getName(ActionEvent actionEvent) {
         String newFname = firstName.getText();
         String newLname = lastName.getText();
@@ -28,5 +52,5 @@ public class FormController {
 
         circle.setCenterX(100);
         circle.setCenterY(100);
-    }
+    }*/
 }
